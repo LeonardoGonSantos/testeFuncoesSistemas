0a29dece552476567d09cac24de2282a7e4dd6b4
leonardoRhaissa <leonardo.estagio.07.11@gmail.com>
2021-02-15 21:13:52 -0300

    Add project files.

---
+++ FI.AtividadeEntrevista/DAL/Clientes/Procedures/FI_SP_PesqBeneficiario.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROC FI_SP_PesqCliente
+	@idCliente bigint
+AS
+BEGIN
+	DECLARE @SCRIPT NVARCHAR(MAX)
+	DECLARE @CAMPOS NVARCHAR(MAX)
+	DECLARE @ORDER VARCHAR(50)
+	
+	IF(@campoOrdenacao = 'EMAIL')
+		SET @ORDER =  ' EMAIL '
+	ELSE
+		SET @ORDER = ' NOME '
+
+	IF(@crescente = 0)
+		SET @ORDER = @ORDER + ' DESC'
+	ELSE
+		SET @ORDER = @ORDER + ' ASC'
+
+	SET @CAMPOS = '@iniciarEm int,@quantidade int'
+	SET @SCRIPT = 
+	'SELECT ID, CPF, NOME FROM
+		(SELECT ROW_NUMBER() OVER (ORDER BY ' + @ORDER + ') AS Row, ID, CPF, NOME FROM CLIENTES WITH(NOLOCK))
+		AS ClientesWithRowNumbers
+	WHERE Row > @iniciarEm AND Row <= (@iniciarEm+@quantidade) ORDER BY'
+	
+	SET @SCRIPT = @SCRIPT + @ORDER
+			
+	EXECUTE SP_EXECUTESQL @SCRIPT, @CAMPOS, @iniciarEm, @quantidade
+
+	SELECT COUNT(1) FROM CLIENTES WITH(NOLOCK)
+END
